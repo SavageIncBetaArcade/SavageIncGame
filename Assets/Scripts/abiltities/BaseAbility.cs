6154433810b1480ce9b2cc4665eda092dc2bfc60
Tieran <tieran.wightman@gmail.com>
2020-10-04 11:15:45 +0100

    Base ability class

---
+++ Assets/Scripts/abiltities/BaseAbility.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BaseAbility : MonoBehaviour
+{
+    public string AbilityName;
+    public float Damage;
+    public float Cooldown;
+
+    //TODO pass in the attackers character base
+    public delegate void AttackAction();
+    public event AttackAction OnAttack;
+
+    public delegate void HitAction();
+    public event HitAction OnHit;
+
+    //TODO get character base on awake
+    //public CharacterBase CharacterBase;
+
+    private float _lastAttackTime;
+
+    protected virtual void Awake()
+    {
+        Cooldown = 0.0f;
+
+        //if(CharacterBase == null)
+        //    Debug.LogWarning($"{AbilityName} doesn't have a character base, please make sure to set it");
+    }
+
+    protected virtual void Update()
+    {
+        if (!OnCooldown() && Input.GetButtonDown("Fire1"))
+        {
+            Attack();
+            _lastAttackTime = Time.time;
+        }
+    }
+
+    protected bool OnCooldown()
+    {
+        return !(Time.time >= _lastAttackTime + Cooldown);
+    }
+
+    public virtual void Attack()
+    {
+        OnAttack?.Invoke();
+    }
+}
---
+++ Assets/Scripts/abiltities/MeleeAbility.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class MeleeAbility : BaseAbility
+{
+    public Animation AttackAnimation;
+    private BoxCollider _hitCollider;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _hitCollider = GetComponent<BoxCollider>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+
+        Debug.Log("Attacking");
+    }
+}
