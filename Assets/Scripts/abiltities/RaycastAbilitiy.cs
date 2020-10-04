ef4571bf9ddb3c276a0e0fdd66b7e1cef5f1c82c
Tieran <tieran.wightman@gmail.com>
2020-10-04 15:48:31 +0100

    basic raycast ability

---
+++ Assets/Scripts/abiltities/RaycastAbilitiy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastAbilitiy : BaseAbility
+{
+    public float Range = 25.0f;
+    public float HitDelay = 0.0f;
+    public Transform RayOrigin;
+
+    public override void Attack()
+    {
+        base.Attack();
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(RayOrigin.position, RayOrigin.forward, out hitInfo, Range))
+        {
+            //First check if it has a health component
+            Hit();
+            
+        }
+
+
+        Debug.Log("Attacking");
+    }
+
+    public override void Hit()
+    {
+        base.Hit();
+
+        Debug.Log("Hit");
+    }
+}
