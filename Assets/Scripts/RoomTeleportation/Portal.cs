19b668d02ea68aa2fc0896d5f913441e7d1c0dde
Gabriel <gabrielmenezes0297@gmail.com>
2020-10-05 22:47:34 +0100

    Room Teleportation camera setup

---
+++ Assets/Scripts/RoomTeleportation/Portal.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    
+    public Portal linkedPortal;
+    public MeshRenderer screen;
+    Camera portalCam;
+    Camera playerCam;
+
+    RenderTexture viewTexture;
+    private MeshFilter screenMeshFilter;
+
+    public float nearClipOffset;
+    public float nearClipLimit;
+
+    void Awake()
+    {
+        playerCam = Camera.main;
+        portalCam = GetComponentInChildren<Camera>();
+        portalCam.enabled = false;
+        screenMeshFilter = screen.GetComponent<MeshFilter> ();
+        screen.material.SetInt ("displayMask", 1);
+    }
+
+
+    void CreateViewTexture()
+    {
+        if (viewTexture == null || viewTexture.width != Screen.width || viewTexture.height != Screen.height)
+        {
+            if (viewTexture != null)
+            {
+                viewTexture.Release();
+            }
+            viewTexture = new RenderTexture(Screen.width, Screen.height, 0);
+            // Render the view from the portal camera to the view texture
+            portalCam.targetTexture = viewTexture;
+            // Display the view texture on the screen of the linked portal
+            linkedPortal.screen.material.SetTexture("_MainTex", viewTexture);
+        }
+    }
+
+    public void Render()
+    {
+        CreateViewTexture();
+
+        var localToWorldMatrix = playerCam.transform.localToWorldMatrix;
+    
+        localToWorldMatrix = transform.localToWorldMatrix * linkedPortal.transform.worldToLocalMatrix * localToWorldMatrix;
+        var renderPositions = localToWorldMatrix.GetColumn(3);
+        var renderRotations = localToWorldMatrix.rotation;
+        
+        portalCam.transform.SetPositionAndRotation(renderPositions, renderRotations);
+           
+        // Hide screen so that camera can see through portal
+        screen.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+        linkedPortal.screen.material.SetInt("displayMask", 0);
+
+      
+        portalCam.transform.SetPositionAndRotation(renderPositions, renderRotations);
+        SetNearClipPlane();
+        //HandleClipping();
+        portalCam.Render();
+
+            
+        linkedPortal.screen.material.SetInt("displayMask", 1);
+            
+        // Unhide objects hidden at start of render
+        screen.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+    }
+
+    void SetNearClipPlane()
+    {
+        Transform clipPlane = transform;
+        int dot = System.Math.Sign(Vector3.Dot(clipPlane.forward, transform.position - portalCam.transform.position));
+
+        Vector3 camSpacePos = portalCam.worldToCameraMatrix.MultiplyPoint(clipPlane.position);
+        Vector3 camSpaceNormal = portalCam.worldToCameraMatrix.MultiplyVector(clipPlane.forward) * dot;
+        float camSpaceDst = -Vector3.Dot(camSpacePos, camSpaceNormal) + nearClipOffset;
+
+        // Don't use oblique clip plane if very close to portal as it seems this can cause some visual artifacts
+        if (Mathf.Abs(camSpaceDst) > nearClipLimit)
+        {
+            Vector4 clipPlaneCameraSpace = new Vector4(camSpaceNormal.x, camSpaceNormal.y, camSpaceNormal.z, camSpaceDst);
+
+            // Update projection based on new clip plane
+            // Calculate matrix with player cam so that player camera settings (fov, etc) are used
+            portalCam.projectionMatrix = playerCam.CalculateObliqueMatrix(clipPlaneCameraSpace);
+        }
+        else
+        {
+            portalCam.projectionMatrix = playerCam.projectionMatrix;
+        }
+    }
+
+    private void Update()
+    {
+        Render();
+    }
+}
+
+  
