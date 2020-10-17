2c0ddc6556c3501a79fdefabb67a0f821be6edd4
plurple <mattgrierson@btinternet.com>
2020-10-17 17:33:07 +0100

    AI state base, state machine and start of AIBase

---
+++ Assets/Scripts/AI/AIBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIBase : CharacterBase
+{
+    public Vector3[] patrolPoints;
+    private int currentPatrolPoint = 0;
+    private NavMeshAgent navAgent;
+    private StackFSM stackOfStates;
+    public State[] potentialStates;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        stackOfStates = GetComponent<StackFSM>();
+    }
+
+}
---
+++ Assets/Scripts/AI/State.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class State : ScriptableObject
+{
+    public virtual void OnPush()
+    {
+
+    }
+
+    public virtual void OnPop()
+    {
+
+    }
+
+    public abstract void OnUpdate();
+}
