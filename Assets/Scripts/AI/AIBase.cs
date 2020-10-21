90110022b898d5164015843a1db5e49cac55bf3b
plurple <mattgrierson@btinternet.com>
2020-10-21 11:51:43 +0100

    AIBase helpful functions

--- Assets/Scripts/AI/AIBase.cs
+++ Assets/Scripts/AI/AIBase.cs
@@ -5,11 +5,14 @@ using UnityEngine.AI;
 
 public class AIBase : CharacterBase
 {
+    public float senseRange = 60.0f;
+    public float angleOfVision = 45.0f;
     public Vector3[] patrolPoints;
     private int currentPatrolPoint = 0;
     private NavMeshAgent navAgent;
     private StackFSM stackOfStates;
     public State[] potentialStates;
+    GameObject player;
 
 
     // Start is called before the first frame update
@@ -19,4 +22,33 @@ public class AIBase : CharacterBase
         stackOfStates = GetComponent<StackFSM>();
     }
 
+    public bool LineSightToPlayer()
+    {
+        if (player && PlayerInFieldOfVision())
+        {
+            if (Physics.Raycast(transform.position, transform.forward, senseRange))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool PlayerInFieldOfVision()
+    {
+        if(player)
+        {
+            Vector3 vectorBetweenThisAndPlayer = player.transform.position - transform.position;
+            if(vectorBetweenThisAndPlayer.magnitude <= senseRange && Vector3.Angle(vectorBetweenThisAndPlayer, transform.forward) <= angleOfVision)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void GetPlayer()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
 }
