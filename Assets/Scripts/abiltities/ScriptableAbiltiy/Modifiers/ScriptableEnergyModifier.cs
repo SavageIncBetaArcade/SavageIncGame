91504a59c7728d6cbaa12b618da3348382dd1e79
tieran02 <tieran.wightman@gmail.com>
2020-10-23 13:26:29 +0100

    Added Heal/Energy Modifiers

---
+++ Assets/Scripts/abiltities/ScriptableAbiltiy/Modifiers/ScriptableEnergyModifier.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AbilityModifiers/EnergyModifier")]
+public class ScriptableEnergyModifier : ScriptableModifier
+{
+    public float Amount = 5.0f;
+    public bool Percentage = false;
+    public bool ReplenishOwner = false;
+
+    public override void OnApply(CharacterBase ownerCharacter, CharacterBase targetCharacter, ref List<CharacterBase> affectedCharacters)
+    {
+        CharacterBase target = ReplenishOwner ? ownerCharacter : targetCharacter;
+        float amount = Percentage ? target.MaxEnergy * Amount : Amount;
+        target.ReplenishEnergy(amount);
+        ApplyEffects(target);
+    }
+
+    public override void OnRemove(CharacterBase ownerCharacter, CharacterBase targetCharacter, ref List<CharacterBase> affectedCharacters)
+    {
+
+    }
+
+    public override void OnTick(CharacterBase ownerCharacter, CharacterBase targetCharacter, ref List<CharacterBase> affectedCharacters)
+    {
+
+    }
+}
+
---
+++ Assets/Scripts/abiltities/ScriptableAbiltiy/Modifiers/ScriptableHealModifier.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AbilityModifiers/HealthModifier")]
+public class ScriptableHealModifier : ScriptableModifier
+{
+    public float Amount = 5.0f;
+    public bool Percentage = false;
+    public bool HealOwner = false;
+
+    public override void OnApply(CharacterBase ownerCharacter, CharacterBase targetCharacter, ref List<CharacterBase> affectedCharacters)
+    {
+        CharacterBase target = HealOwner ? ownerCharacter : targetCharacter;
+        float amount = Percentage ? target.MaxHealth * Amount : Amount;
+        target.Heal(amount);
+        ApplyEffects(target);
+    }
+
+    public override void OnRemove(CharacterBase ownerCharacter, CharacterBase targetCharacter, ref List<CharacterBase> affectedCharacters)
+    {
+
+    }
+
+    public override void OnTick(CharacterBase ownerCharacter, CharacterBase targetCharacter, ref List<CharacterBase> affectedCharacters)
+    {
+
+    }
+}
+
