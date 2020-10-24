1943466eb0dbf676dbb55a1e4a3112012bb40dad
plurple <mattgrierson@btinternet.com>
2020-10-24 16:25:33 +0100

    AI patrol improvements, adding the movementState to transition to the attack states when player get spotted, added the follow state to follow the player, added random patrol if no patrol points set up.

---
+++ Assets/Scripts/AI/States/MovingState.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public abstract class MovingState : State
+{
+    public override void OnUpdate(ref StackFSM stackStates)
+    {
+        if(stackStates.aiBase.PlayerInFieldOfVision())
+        {
+            State follow = stackStates.aiBase.PotentialStates.FirstOrDefault(x => x.StateName == StateNames.FollowState);
+            if (follow)
+                stackStates.PushState(follow);
+        }
+    }
+}
