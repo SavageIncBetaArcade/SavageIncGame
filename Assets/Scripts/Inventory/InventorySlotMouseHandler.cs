6642015f206e1cc3de5808db6f1bd7d3b3182813
Joe <t7051015@live.tees.ac.uk>
2020-11-05 21:53:48 +0000

    Refactor

--- Assets/Scripts/Inventory/InventorySlotMouseHandler.cs
+++ Assets/Scripts/Inventory/InventorySlotMouseHandler.cs
@@ -1,20 +1,11 @@
-﻿using UnityEngine;
-using UnityEngine.EventSystems;
+﻿using UnityEngine.EventSystems;
 
-public class InventorySlotMouseHandler : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+public class InventorySlotMouseHandler : InfoPopupHandler, IPointerClickHandler
 {
     public InventorySectionHandler inventorySectionHandler;
     private Inventory Inventory => inventorySectionHandler.currentInventory;
     public int position;
-    private Item Item => Inventory.getItemAt(position);
-    public GameObject itemInfoPrefab;
-    private Popup popup;
-    private Canvas canvas;
-
-    private void Awake()
-    {
-        canvas = FindObjectOfType<Canvas>();
-    }
+    protected override Item Item => Inventory.getItemAt(position);
 
     public void OnPointerClick(PointerEventData eventData)
     {
@@ -27,39 +18,5 @@ public class InventorySlotMouseHandler : MonoBehaviour, IPointerClickHandler, IP
                 Inventory.RightClickItem(position);
                 break;
         }
-    }
-
-    public void OnPointerEnter(PointerEventData data)
-    {
-        if(Inventory.hasItemAt(position)) ShowItemInfo();
-    }
-
-    public void OnPointerExit(PointerEventData data)
-    {
-        if(popup) Destroy(popup.gameObject);
-    }
-    
-    private void ShowItemInfo()
-    {
-        popup = Instantiate(itemInfoPrefab).GetComponent<Popup>();
-        popup.title.text = Item.Name;
-        popup.quote.text = Item.Quote;
-        popup.description.text = Item.GetInfoDescription();
-        popup.transform.position = new Vector3(transform.position.x + GetPopupXOffset(), transform.position.y, 0);
-        popup.transform.parent = canvas.transform;
-    }
-
-    private float GetPopupXOffset()
-    {
-        var slotWidth = GetComponent<RectTransform>().rect.width;
-        var popupWidth = popup.GetComponent<RectTransform>().rect.width - slotWidth * 0.2f;
-        if (CanvasContainsPopup(popupWidth))
-            return popupWidth;
-        return -popupWidth;
-    }
-
-    private bool CanvasContainsPopup(float popupWidth)
-    {
-        return canvas.GetComponent<RectTransform>().rect.Contains(new Vector2(transform.position.x + popupWidth, transform.position.y));
     }
 }
