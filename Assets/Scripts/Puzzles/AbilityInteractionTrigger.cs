6fbf8e5174409781980d2eb45de05bed12037701
tieran02 <tieran.wightman@gmail.com>
2020-11-18 18:55:26 +0000

    Ability interaction triggers

---
+++ Assets/Scripts/Puzzles/AbilityInteractionTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class AbilityInteractionTrigger : InteractionTrigger
+{
+    public ScriptableUseableAbility[] Abilities;
+
+    //Hit detection is in AttacjAbility.cs
+}
