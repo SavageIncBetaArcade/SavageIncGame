405c6e76d61a08f5606184a4f70253d5bc7a565c
tieran02 <tieran.wightman@gmail.com>
2020-11-20 15:51:15 +0000

    Falling Ceilings

---
+++ Assets/Scripts/DamageVolume.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageVolume : MonoBehaviour
+{
+    public float DamageAmount;
+    public bool ContinuousDamage;
+    public float Timer;
+
+    private float nextDamageTimer;
+    private bool damagedTaken;
+
+    void Awake()
+    {
+        nextDamageTimer = Timer;
+    }
+
+    void Damage(GameObject gameObject)
+    {
+        if (!damagedTaken && nextDamageTimer >= Timer)
+        {
+            CharacterBase character = GetCharacterFromParent(gameObject.transform);
+            character.TakeDamage(DamageAmount);
+            damagedTaken = true;
+            if(ContinuousDamage)
+                StartCoroutine(resetDamage());
+        }
+
+        nextDamageTimer += Time.deltaTime;
+    }
+
+    private CharacterBase GetCharacterFromParent(Transform currentTransform)
+    {
+        CharacterBase character = currentTransform.GetComponent<CharacterBase>();
+
+        if (character)
+            return character;
+
+        if (!currentTransform.parent)
+            return null;
+
+        return GetCharacterFromParent(currentTransform.parent);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        Damage(collision.gameObject);
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        Damage(collider.gameObject);
+    }
+
+    IEnumerator resetDamage()
+    {
+        yield return new WaitForFixedUpdate();
+        damagedTaken = false;
+    }
+}
---
+++ Assets/Scripts/InteractionTriggers/TriggeredFallingObject.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TriggeredFallingObject : MonoBehaviour
+{
+    public InteractionTrigger[] Triggers;
+
+    public float FallDelay;
+    public float DestroyDelay;
+
+    private Rigidbody rigidbody;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.isKinematic = true;
+
+        foreach (var trigger in Triggers)
+        {
+            trigger.OnTrigger += Trigger;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Trigger(bool triggered)
+    {
+        if (Triggers.All(x => x.Triggered))
+        {
+            StartCoroutine(Fall());
+        }
+    }
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(FallDelay);
+        rigidbody.isKinematic = false;
+    }
+
+}
