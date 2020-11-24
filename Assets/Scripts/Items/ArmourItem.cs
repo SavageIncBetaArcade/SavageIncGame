68dd07a07f01e82ddd581d8f566b99428f931dad
Joe <t7051015@live.tees.ac.uk>
2020-11-24 17:54:39 +0000

    Equippable items have modifiers

--- Assets/Scripts/Items/ArmourItem.cs
+++ Assets/Scripts/Items/ArmourItem.cs
@@ -1,26 +1,6 @@
 ﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "Armour", menuName = "Item/Armour")]
-public class ArmourItem : Item
+public class ArmourItem : EquippableItem
 {
-    public int attack;
-    public int defense;
-    public int health;
-    public int energy;
-    public float healthRegen;
-    public float energyRegen;
-    
-    public override string GetInfoDescription()
-    {
-        return getStatString(attack, "Attack") +
-               getStatString(defense, "Defense") +
-               getStatString(health, "Health") +
-               getStatString(energy, "Energy") +
-               getRegenString(healthRegen, "Health") + 
-               getRegenString(energyRegen, "Energy");
-    }
-
-    private string getStatString(int stat, string statName) { return stat != 0 ? $"+ {stat} {statName}\n" : ""; }
-    
-    private string getRegenString(float stat, string statName) { return stat != 0 ? $"+{stat}/second {statName} Regen\n" : ""; }
 }
