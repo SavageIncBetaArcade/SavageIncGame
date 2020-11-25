941ec94767b2c371cd3884a5cac68eb52fdc2023
Joe <t7051015@live.tees.ac.uk>
2020-11-25 15:59:56 +0000

    Added toggle functionality

---
+++ Assets/Scripts/Menus/Toggle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Toggle : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject yesImage;
+    public GameObject noImage;
+    private bool enabled = true;
+
+    private void Start()
+    {
+        SetActiveImage();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        enabled = !enabled;
+        SetActiveImage();
+    }
+
+    private void SetActiveImage()
+    {
+        yesImage.SetActive(enabled);
+        noImage.SetActive(!enabled);
+    }
+}
