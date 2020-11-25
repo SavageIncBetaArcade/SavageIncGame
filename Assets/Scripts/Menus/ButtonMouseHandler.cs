12c18feee1cca5827bf808254fc6894eb0930796
Joe <t7051015@live.tees.ac.uk>
2020-11-25 01:35:56 +0000

    Main menu fits design

---
+++ Assets/Scripts/Menus/ButtonMouseHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonMouseHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale = new Vector3(0.9f, 0.9f, 0.9f);
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+    }
+}
