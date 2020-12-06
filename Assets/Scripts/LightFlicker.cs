bb6ec739367d48df54f88ccfc5e8ae8bd6b2cb06
tieran02 <tieran.wightman@gmail.com>
2020-12-06 14:47:12 +0000

    Added light flicker and fixed section one probuilder mesh

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LightFlicker : MonoBehaviour
+{
+    public Light light;
+    public float minLumen = 500f;
+    public float maxLumen = 700f;
+    [Range(1, 50)]
+    public int smoothing = 5;
+
+    // Continuous average calculation via FIFO queue
+    // Saves us iterating every time we update, we just change by the delta
+    Queue<float> smoothQueue;
+    float lastSum = 0;
+
+    public void Reset()
+    {
+        smoothQueue.Clear();
+        lastSum = 0;
+    }
+
+    public void Awake()
+    {
+        light = GetComponent<Light>();
+    }
+
+    void Start()
+    {
+        smoothQueue = new Queue<float>(smoothing);
+        // External or internal light?
+        if (light == null)
+        {
+            light = GetComponent<Light>();
+        }
+    }
+
+    void Update()
+    {
+        if (light == null)
+            return;
+
+        // pop off an item if too big
+        while (smoothQueue.Count >= smoothing)
+        {
+            lastSum -= smoothQueue.Dequeue();
+        }
+
+        // Generate random new item, calculate new average
+        float newVal = Random.Range(minLumen, maxLumen);
+        smoothQueue.Enqueue(newVal);
+        lastSum += newVal;
+
+        // Calculate new smoothed average
+        light.intensity = lastSum / (float)smoothQueue.Count;
+    }
+
+}
