2d1e37520d4b402c9316ba60197431452f6b11e0
tieran02 <tieran.wightman@gmail.com>
2020-12-09 14:50:10 +0000

    menu and sounds

--- Assets/Scripts/Menus/MainMenuMouseHandler.cs
+++ Assets/Scripts/Menus/MainMenuMouseHandler.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuMouseHandler : MonoBehaviour
 {
     public GameObject instructionsPanel;
     public GameObject optionsPanel;
     public GameObject creditsPanel;
+    public int GameSceneIndex = 1;
     
     public void OnPlayClicked()
     {
-        
+        SceneManager.LoadSceneAsync(GameSceneIndex);
     }
     
     public void OnInstructionsClicked()
