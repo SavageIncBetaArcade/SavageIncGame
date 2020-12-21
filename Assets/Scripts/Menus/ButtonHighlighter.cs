1aff41bd25c2b8fb216dc4e9d1bbb258af418035
Joe <t7051015@live.tees.ac.uk>
2020-12-21 16:13:32 +0000

    Buttons highlight

---
+++ Assets/Scripts/Menus/ButtonHighlighter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonHighlighter : MonoBehaviour
+{
+    public Sprite normalImage;
+    public Sprite highlightImage;
+    public Image buttonImage;
+    
+    private void Update()
+    {
+        ChangeButtonImage(EventSystem.current.currentSelectedGameObject == gameObject ? highlightImage : normalImage);
+    }
+
+    private void ChangeButtonImage(Sprite image)
+    {
+        buttonImage.sprite = image;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ChangeButtonImage(highlightImage);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ChangeButtonImage(normalImage);
+    }
+}
