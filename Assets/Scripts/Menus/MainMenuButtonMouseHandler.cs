a8c621f9136b55badce0c14e0e6f3f314c4134c0
Joe <t7051015@live.tees.ac.uk>
2020-12-21 15:19:51 +0000

    Controller support added for main menu

--- Assets/Scripts/Menus/MainMenuButtonMouseHandler.cs
+++ Assets/Scripts/Menus/MainMenuButtonMouseHandler.cs
@@ -3,13 +3,23 @@ using UnityEngine.EventSystems;
 
 public class MainMenuButtonMouseHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    private void Update()
+    {
+        ChangeButtonScale(EventSystem.current.currentSelectedGameObject == gameObject ? 0.9f : 1.0f);
+    }
+
+    private void ChangeButtonScale(float scale)
+    {
+        transform.localScale = new Vector3(scale, scale, scale);
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
-        transform.localScale = new Vector3(0.9f, 0.9f, 0.9f);
+        ChangeButtonScale(0.9f);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        transform.localScale = new Vector3(1, 1, 1);
+        ChangeButtonScale(1.0f);
     }
 }
