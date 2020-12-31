8bc82179392e0ebf122f296fe8b13f9231808ddc
tieran02 <tieran.wightman@gmail.com>
2020-12-31 19:02:22 +0000

    boss room death trigger

---
+++ Assets/Scripts/InteractionTriggers/DeathInteractionTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class DeathInteractionTrigger : InteractionTrigger
+{
+    private CharacterBase characterBase;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        characterBase = GetComponent<CharacterBase>();
+        characterBase.OnDeath += onDeath;
+    }
+
+    void OnDestroy()
+    {
+        characterBase.OnDeath -= onDeath;
+    }
+
+    void onDeath()
+    {
+        Interact();
+    }
+}
